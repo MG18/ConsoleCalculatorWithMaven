2c3a99e85ed376e2e42535456fbbc06ae65083a3
5ia16meguel <5ia16meguel@gibwintra.ch>
2018-12-07 08:14:41 +0100

    @Before setUp method Implemented in Unittest

--- src/test/java/ch/bbw/mg/Junit_Test/CalculatorTest.java
+++ src/test/java/ch/bbw/mg/Junit_Test/CalculatorTest.java
@@ -2,11 +2,19 @@ package ch.bbw.mg.Junit_Test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runners.model.TestTimedOutException;
 
 public class CalculatorTest {
 	Calculator testee;
+	
+	
+	@Before
+	public void setUp() {
+		Calculator testee = new Calculator();
+	}
+	
 
 	@Test
 	public void testSummeZweiPositiveIsOk() {
